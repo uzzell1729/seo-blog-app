a5ee7e4fe4a392c48998361974a3ce462d676859
uzzell1729 <39248868+uzzell1729@users.noreply.github.com>
2026-03-10 19:27:09 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname),
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
