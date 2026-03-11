6d400f45b8d65ddf5708b4cd4e5cb06971ab1db6
uzzell1729 <39248868+uzzell1729@users.noreply.github.com>
2026-03-10 19:10:20 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,  // !! Only for testing – remove later !!
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname),
+    };
+    return config;
   },
 };
 
