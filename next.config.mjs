2f8090b748851bcf1332a0b1e896f5b5f333f889
uzzell1729 <39248868+uzzell1729@users.noreply.github.com>
2026-03-10 19:39:00 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
-import path from 'path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
+      '@': __dirname,
     };
     return config;
   },
